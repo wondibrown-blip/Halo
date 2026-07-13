8b3f35b028ff3b1ed8a6c3e4b5e95164f44aaa22
wondibrown-blip <wondibrown@gmail.com>
2026-07-13 18:24:34 +0700

    Update react.py

--- react.py
+++ react.py
@@ -95,10 +95,10 @@ async def cmd_doni(client: Client, message: Message):
     
     # Template Estetik SYNC PACT sesuai permintaan
     caption_template = (
-        " ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ\n"
+        "``` ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ\n"
         "𝗦𝗬𝗡𝗖 𝗣𝗔𝗖𝗧: Mutual terms enforced.\n"
-        f"Alignment forms beyond consent, presence settles refuses 2 shift. 𝘕𝘰 𝘧𝘳𝘢𝘤𝘵𝘶𝘳𝘦 𝘰𝘯𝘤𝘦 𝘪𝘵 𝘴𝘦𝘵𝘴: {usernames_string} (*{total_react_count}) bound. Balanced force, no reversal (+KT9.) https://t.me/HOTROVERs/4.\n"
-        "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ"
+        f"Alignment forms beyond consent, presence settles refuses 2 shift. 𝘕𝘰 𝘧𝘳𝘢𝘤𝘵𝘶𝘳𝘦 𝘰𝘯𝘤𝘦 𝘪𝘵 𝘴𝘦𝘵𝘴: {usernames_string} ({total_react_count}) bound. Balanced force, no reversal (+KT9.) https://t.me/HOTROVERs/4.\n"
+        "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ```"
     )
     
     await message.reply_text(text=caption_template, disable_web_page_preview=False)
